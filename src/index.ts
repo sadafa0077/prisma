0efbe6e14239ec0cda44b369ff0a9607b718ee0b
sadafa0077 <densi007kw@gmail.com>
2021-11-19 06:48:07 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
 const prisma = new PrismaClient();
 
 app.post("/", async (req: Request, res: Response) => {
-    const { todo } = req.body; //リクエストの中身を定数に代入
-    const todos = await prisma.user.createMany({ //複数の新規データを作成する
-        data: todo, //todoに代入する
-    })
-    res.json(todos);　//json形式で送信する
-
+    const { todo } = req.body;
+    const todos = await prisma.user.create({
+        data: todo,
+    });
+    res.json(todos);
 });
